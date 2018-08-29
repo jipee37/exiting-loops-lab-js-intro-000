9e53cd2cb9513e34efa41458a36f0a743220921c
Jean-Patrice <jpjocegilbert@gmail.com>
2018-08-29 15:52:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 function breakOut(array, changeValue, stopValue){
   for(let i=0; i< array.length; ++i){
       if(array[i] !== stopValue) {
-		array[i] = changeValue
+		      array[i] = changeValue
       } else {
 	  	    break
       }
